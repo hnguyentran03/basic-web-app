6f9dca78bba61988ba548323f03efe15852f51d9
Ben <hnguyent@andrew.cmu.edu>
2024-03-14 15:45:38 -0400

    changed andrewid

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,7 +6,7 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   } else if (query.toLowerCase().includes("andrew id")) {
-    return ("My Andrew ID hnguyent.");
+    return ("hnguyent");
   } else if (query.toLowerCase().includes("name")) {
     return ("Ben")
   }
